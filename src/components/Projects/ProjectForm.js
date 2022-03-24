699bbe0841d39d5c588e9418378d22bf9ee1ad4a
Marcelo Ludwig Junior <marttelo@outlook.com>
2022-03-24 04:33:43 -0300

    conectando com a API json-server

--- src/components/Projects/ProjectForm.js
+++ src/components/Projects/ProjectForm.js
@@ -2,20 +2,41 @@ import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
+import { useState, useEffect } from 'react';
 
-export default function ProjectForm({btnText}) {
+
+
+export default function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'applicatiom/json',
+        },
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data);
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
     return (
         <form className={styles.form}>
             <div>
-                <Input type="text" text="Mês de referência" name="name" placeholder="Informe o mês" />
+                <Input type="text" text="Mês de referência" name="name" placeholder="Informe o  mês" />
             </div>
             <div>
                 <Input type="number" text="Orçamento Total" name="budget" placeholder="Informe o  valor total do orçamento para o mês" />
             </div>
             <div>
-                <Select name="categoryId" text="Selecione a Categoria"></Select>
+                <Select name="category_id" text="Selecione a Categoria" options={categories} />
             </div>
-            <br/>
+            <br />
             <div>
                 <SubmitButton text={btnText} />
             </div>
--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,12 +1,17 @@
 import styles from './Select.module.css'
 
 
-export default function Select({text, name, option, handleOnChange, value}){
+export default function Select({text, name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
-            <option value={name}>Selecione uma opção: </option>
+            <option>Selecione uma opção: </option>
+            {options.map((option) => (
+                <option value={option.id} key={option.id}>
+                    {option.name}
+                </option>
+            ))}
             </select>
         </div>
     )
